e8a17306c7b9330df8c1503b9da03b275e5abf9c
ISHANT KUMAR YADAV <114832189+yishant11@users.noreply.github.com>
2025-08-08 01:20:25 +0530

    done

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import VideoTimelineBrowser from './components/VideoTimelineBrowser';
-import './App.css';
-function App() {
-    return (_jsx("div", { className: "App", children: _jsx(VideoTimelineBrowser, {}) }));
-}
-export default App;
